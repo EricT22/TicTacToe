1e9998189538ad0278fa72f8928301b711f87c73
eric <phantomenace87@gmail.com>
2024-03-25 19:13:33 -0700

    added win feature and color change on win for a diagonal, the rest tbc

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ let boardVals = [[-1,-1,-1],
                  [-1,-1,-1], 
                  [-1,-1,-1]];
 
+let gameOver = false;
 let turn = 0; // 0 for X, 1 for O
+
 
 for (let tile of document.querySelectorAll(".board-tile")){
     tile.addEventListener("click", handleClick);
 }
 
 function handleClick(event){
-    let curID = event.currentTarget.getAttribute("id");
-
-    let firstInd;
-    let secondInd;
+    if (gameOver){
+        resetBoard();
+        return;
+    }
     let numTilesFull = 0;
-
+    
     for (let i = 0; i < boardVals.length; i++){
         for (let j = 0; j < boardVals[i].length; j++){
             if (boardVals[i][j] != -1){
@@ -22,11 +24,16 @@ function handleClick(event){
             }
         }
     }
-
+    
     if (numTilesFull == 9){
         resetBoard();
         return;
     }
+
+    let curID = event.currentTarget.getAttribute("id");
+
+    let firstInd;
+    let secondInd;
 
 
     if (curID.includes("top")){
@@ -45,9 +52,13 @@ function handleClick(event){
         secondInd = 2;
     }
 
-    boardVals[firstInd][secondInd] = (turn % 2 == 0) ? 0 : 1;
-    document.getElementById(curID).firstElementChild.textContent = (turn % 2 == 0) ? "X" : "O";
-    turn++;
+    if (boardVals[firstInd][secondInd] == -1) {
+        boardVals[firstInd][secondInd] = (turn % 2 == 0) ? 0 : 1;
+        document.getElementById(curID).firstElementChild.textContent = (turn % 2 == 0) ? "X" : "O";
+        
+        checkWin();
+        turn++;
+    }
 }
 
 function resetBoard(){
@@ -59,5 +70,47 @@ function resetBoard(){
 
     tiles.forEach((element) => {
         element.textContent = "";
+        element.style.color = "black";
     });
+
+    turn = 0;
+    gameOver = false;
+}
+
+function checkWin(){
+    let curPiece = turn % 2;
+    
+    for (let i = 0; i < 3; i++){
+        if (boardVals[i][0] == curPiece && boardVals[i][1] == curPiece && boardVals[i][2] == curPiece){
+            //highlight row
+
+            return;
+        }
+    }
+
+    for (let i = 0; i < 3; i++){
+        if (boardVals[0][i] == curPiece && boardVals[1][i] == curPiece && boardVals[2][i] == curPiece){
+            //highlight column
+
+            return;
+        }
+    }
+
+    if (boardVals[0][0] == curPiece && boardVals[1][1] == curPiece && boardVals[2][2] == curPiece){
+        let spans = document.querySelectorAll("#top-left > span, #center-middle > span, #bottom-right > span");
+        console.log(spans);
+
+        spans.forEach((element) => {
+            element.style.color = "red";
+        });
+
+        gameOver = true;
+        return;
+    }
+    
+    if (boardVals[0][2] == curPiece && boardVals[1][1] == curPiece && boardVals[2][0] == curPiece){
+        //highlight diag
+
+        return;
+    }
 }
